a72ea475712d43909820064a522d21ac22cf8dd7
rimadas123 <rdas5969@gmail.com>
2020-03-18 15:05:47 +0530

    Added : to check incorrect file name returns file not found exception

--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -1,8 +1,10 @@
 import com.census_analyser.StateCensusAnalyser;
+import com.census_analyser.StateCensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 
 public class StateCensusAnalyserTest {
 
@@ -10,9 +12,19 @@ public class StateCensusAnalyserTest {
     StateCensusAnalyser analyser=new StateCensusAnalyser();
 
     @Test
-    public void givenTheStatesCensusCSVFiles_CheckToEnsureTheNumber_OfRecordMatches() throws IOException {
+    public void givenTheStatesCensusCSVFiles_CheckToEnsureTheNumber_OfRecordMatches() throws IOException, StateCensusAnalyserException {
         int count = analyser.readCSVFile("./src/test/resources/StateCensusData.csv");
         Assert.assertEquals(29,count);
+    }
+
+    @Test
+    public void givenTheStateCensus_CSVFile_IfIncorrectReturnsACustomException() throws IOException, StateCensusAnalyserException {
+        try {
+            int fileName = analyser.readCSVFile("./src/test/resources/StateCensus.csv");
+            Assert.assertEquals("29", fileName);
+        } catch (StateCensusAnalyserException e){
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.FILE_NOT_FOUND,e.exceptiontype);
+        }
     }
     
 }
