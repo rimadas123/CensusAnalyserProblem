be04426b89cb70b8ca68926d5fcfbadc571262e5
rimadas123 <rdas5969@gmail.com>
2020-03-19 17:47:22 +0530

    Refactored : the code as DRY was violated to extract data from opencsv

--- src/main/java/com/census_analyser/StateCensusAnalyser.java
+++ src/main/java/com/census_analyser/StateCensusAnalyser.java
@@ -1,5 +1,7 @@
 package com.census_analyser;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
@@ -8,51 +10,74 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 public class StateCensusAnalyser {
 
-    public int readCSVFile(String CSV_PATH) throws StateCensusAnalyserException {
-        int count = 0;
-        String fileName = getFileExtension(CSV_PATH);
-        if(!fileName.equals(".csv"))
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE,"file does not exists");
-        try(Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));
-            CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
-        ){
-            String[] records;
-            while((records = csvReader.readNext()) !=null) {
-                count++;
-                System.out.println("State : "+records[0]);
-                System.out.println("Population : "+records[1]);
-                System.out.println("AreaInSqKm : "+records[2]);
-                System.out.println("DensityPerSqKm : "+records[3]);
-                System.out.println("==================");
-            }
-        } catch (RuntimeException e){
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.WRONG_DELIMITER,"wrong delimiter used in file");
-        }
-        catch (NoSuchFileException e) {
-            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.FILE_NOT_FOUND,"File not found");
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        return count;
-    }
-
-    public static String getFileExtension(String file){
+    public static String getFileExtension(String file) {
         String fileName = "";
-        try{
-            if(file != null){
-                fileName=file.substring(file.lastIndexOf("."));
+        try {
+            if (file != null) {
+                fileName = file.substring(file.lastIndexOf("."));
             }
-        } catch (Exception e){
-            fileName="";
+        } catch (Exception e) {
+            fileName = "";
         }
         return fileName;
     }
 
-    public static void main(String[] args){
-        System.out.println("Welcome to Census Analyser Problem");
+    private <E> Iterator<E> getCsvFileIterator(Reader reader, Class<E> csvClass) {
+        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+        csvToBeanBuilder.withType(csvClass);
+        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+        return csvToBean.iterator();
+    }
+
+    public int readCSVFile(String CSV_PATH) throws StateCensusAnalyserException, IOException {
+        int count = 0;
+        String fileName = getFileExtension(CSV_PATH);
+        if (!fileName.equals(".csv"))
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE, "file does not exists");
+        try (Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH))
+        ) {
+            Iterator<StateCensusData> censusDataIterator = this.getCsvFileIterator(reader, StateCensusData.class);
+
+            while (censusDataIterator.hasNext()) {
+                count++;
+                StateCensusData csvData = censusDataIterator.next();
+                System.out.println("State : " + csvData.State);
+                System.out.println("Population : " + csvData.Population);
+                System.out.println("AreaInSqKm : " + csvData.AreaInSqKm);
+                System.out.println("DensityPerSqKm : " + csvData.DensityPerSqKm);
+                System.out.println("===============");
+            }
+
+            try (Reader reader1 = Files.newBufferedReader(Paths.get(CSV_PATH));
+            ) {
+                CsvToBean<StateCodeData> csvToBean = new CsvToBeanBuilder<StateCodeData>(reader1)
+                        .withType(StateCodeData.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+                Iterator<StateCodeData> codeDataIterator = csvToBean.iterator();
+
+                while (censusDataIterator.hasNext()) {
+                    count++;
+                    StateCodeData csvData = codeDataIterator.next();
+                    System.out.println("Sr no : " + csvData.SrNo);
+                    System.out.println("State Name : " + csvData.StateName);
+                    System.out.println("TIN : " + csvData.TIN);
+                    System.out.println("State Code : " + csvData.StateCode);
+                    System.out.println("===============");
+                }
+            } catch (RuntimeException e) {
+                throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.WRONG_DELIMITER, "wrong delimiter used in file");
+            } catch (NoSuchFileException e) {
+                throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.FILE_NOT_FOUND, "File not found");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return count;
+        }
     }
 }
