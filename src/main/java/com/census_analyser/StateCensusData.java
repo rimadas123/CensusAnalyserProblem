e778a0e9adab4030fd779318ac20cf1e628a9b34
rimadas123 <rdas5969@gmail.com>
2020-03-19 17:49:07 +0530

    Added : pojo class for state census analyser

--- src/main/java/com/census_analyser/StateCensusData.java
+++ src/main/java/com/census_analyser/StateCensusData.java
@@ -1,4 +1,18 @@
 package com.census_analyser;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class StateCensusData {
+
+    @CsvBindByName(column = "State")
+    public String State;
+
+    @CsvBindByName
+    public String Population;
+
+    @CsvBindByName
+    public String AreaInSqKm;
+
+    @CsvBindByName
+    public String DensityPerSqKm;
 }
