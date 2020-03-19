f68536478dc5bdfd7aafa7b16f528b253fcfe60f
rimadas123 <rdas5969@gmail.com>
2020-03-19 17:50:04 +0530

    Added : pojo class for states code

--- src/main/java/com/census_analyser/StateCodeData.java
+++ src/main/java/com/census_analyser/StateCodeData.java
@@ -1,4 +1,18 @@
 package com.census_analyser;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class StateCodeData {
+
+    @CsvBindByName(column = "SrNo")
+    public String SrNo;
+
+    @CsvBindByName
+    public String StateName;
+
+    @CsvBindByName
+    public String TIN;
+
+    @CsvBindByName
+    public String StateCode;
 }
