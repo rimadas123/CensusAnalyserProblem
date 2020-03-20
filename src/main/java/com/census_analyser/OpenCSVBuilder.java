10f766c3698f5d58d9b9b52a139736c5cf976f1a
rimadas123 <rdas5969@gmail.com>
2020-03-20 20:03:18 +0530

    Added : to ensure single responsibility for analyser using delegation principle

---
+++ src/main/java/com/census_analyser/OpenCSVBuilder.java
@@ -0,0 +1,21 @@
+package com.census_analyser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCSVBuilder {
+    public <E> Iterator<E> getCsvFileIterator(Reader reader, Class<E> csvClass) throws StateCensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
+        } catch (IllegalStateException e){
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.UNABLE_TO_PARSE,"unable to parse csv file");
+        }
+    }
+}
