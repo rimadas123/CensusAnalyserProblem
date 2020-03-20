83655ff65f9b07fb13285679d11781a344e6c50d
rimadas123 <rdas5969@gmail.com>
2020-03-20 13:44:17 +0530

    Added : UNABLE_To_PARSE constant in enum method

--- src/main/java/com/census_analyser/StateCensusAnalyserException.java
+++ src/main/java/com/census_analyser/StateCensusAnalyserException.java
@@ -2,7 +2,7 @@ package com.census_analyser;
 
 public class StateCensusAnalyserException extends Throwable {
     public enum ExceptionType {
-        NO_SUCH_FILE,FILE_NOT_FOUND,WRONG_DELIMITER
+        NO_SUCH_FILE,FILE_NOT_FOUND,WRONG_DELIMITER,UNABLE_TO_PARSE
     }
 
     public ExceptionType exceptiontype;
