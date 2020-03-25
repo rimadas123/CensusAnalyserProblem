d551f6fb9e146d7e9c8fc00e65d9a52463bbb8c8
rimadas123 <rdas5969@gmail.com>
2020-03-25 14:03:32 +0530

    Refactored : code to implement Data Access Object

--- src/main/java/com/census_analyser/IndiaCensusDAO.java
+++ src/main/java/com/census_analyser/IndiaCensusDAO.java
@@ -1,4 +1,16 @@
 package com.census_analyser;
 
 public class IndiaCensusDAO {
+
+    public int densityPerSqKm;
+    public int areaInSqKm;
+    public int population;
+    public String state;
+
+    public IndiaCensusDAO(StateCensusData stateCensusData){
+        state = stateCensusData.state;
+        population = stateCensusData.Population;
+        areaInSqKm = stateCensusData.AreaInSqKm;
+        densityPerSqKm = stateCensusData.DensityPerSqKm;
+    }
 }
--- src/main/java/com/census_analyser/StateCodeDAO.java
+++ src/main/java/com/census_analyser/StateCodeDAO.java
@@ -1,4 +1,16 @@
 package com.census_analyser;
 
 public class StateCodeDAO {
+
+    public String stateCode;
+    public String TIN;
+    public String stateName;
+    public int srNo;
+
+    public StateCodeDAO(StateCodeData stateCodeData) {
+        srNo = stateCodeData.SrNo;
+        stateName = stateCodeData.StateName;
+        TIN = stateCodeData.TIN;
+        stateCode = stateCodeData.StateCode;
+    }
 }
