27792b071939b32d1f2c4f89fd42c6c3e082c097
rimadas123 <rdas5969@gmail.com>
2020-03-25 22:46:25 +0530

    Refactored : code to load State Code to Census DAO

---
+++ src/main/java/com/census_analyser/StateCensusDAO.java
@@ -0,0 +1,16 @@
+package com.census_analyser;
+
+public class StateCensusDAO {
+    public int densityPerSqKm;
+    public int areaInSqKm;
+    public int population;
+    public String state;
+    public String stateCode;
+
+    public StateCensusDAO(StateCensusData stateCensusData){
+        state = stateCensusData.state;
+        population = stateCensusData.Population;
+        areaInSqKm = stateCensusData.AreaInSqKm;
+        densityPerSqKm = stateCensusData.DensityPerSqKm;
+    }
+}
