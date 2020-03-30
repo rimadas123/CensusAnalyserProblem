c8013891c9977e21dc33f49c8af3ac456dcb1239
rimadas123 <rdas5969@gmail.com>
2020-03-30 14:36:39 +0530

    Refactored : code to change variable name

--- src/main/java/com/census_analyser/USCensusData.java
+++ src/main/java/com/census_analyser/USCensusData.java
@@ -7,15 +7,15 @@ public class USCensusData {
     @CsvBindByName(column = "StateId",required = true)
     public String StateId;
 
-    @CsvBindByName
+    @CsvBindByName(column ="State",required = true)
     public String state;
 
-    @CsvBindByName
+    @CsvBindByName(column ="Population",required = true)
     public int population;
 
-    @CsvBindByName
+    @CsvBindByName(column = "TotalArea",required = true)
     public double TotalArea;
 
-    @CsvBindByName
+    @CsvBindByName(column="PopulationDensity",required = true)
     public double PopulationDensity;
 }
