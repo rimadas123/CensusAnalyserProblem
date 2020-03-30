172fc5ab12e04251ca824d7098103e7e1fcf8cd7
rimadas123 <rdas5969@gmail.com>
2020-03-30 14:27:49 +0530

    Added : common DAO class to accomodate all data in one place

---
+++ src/main/java/com/census_analyser/CensusDAO.java
@@ -0,0 +1,37 @@
+package com.census_analyser;
+
+public class CensusDAO {
+    public double TotalArea;
+    public double PopulationDensity;
+    public String StateId;
+    public int densityPerSqKm;
+    public int areaInSqKm;
+    public int population;
+    public String state;
+    public String stateCode;
+    public String tin;
+    public int srNo;
+
+    public CensusDAO(StateCensusData stateCensusData){
+        this.state = stateCensusData.state;
+        this.population = stateCensusData.population;
+        this.areaInSqKm = stateCensusData.areaInSqKm;
+        this.densityPerSqKm = stateCensusData.densityPerSqKm;
+    }
+
+    public CensusDAO(StateCodeData stateCodeData){
+        this.srNo = stateCodeData.SrNo;
+        this.state = stateCodeData.StateName;
+        this.tin = stateCodeData.TIN;
+        this.stateCode = stateCodeData.StateCode;
+    }
+
+    public CensusDAO(USCensusData censusCSV) {
+        this.state = censusCSV.state;
+        this.stateCode = censusCSV.StateId;
+        this.population = censusCSV.population;
+        this.PopulationDensity = censusCSV.PopulationDensity;
+        this.TotalArea = censusCSV.TotalArea;
+    }
+
+}
